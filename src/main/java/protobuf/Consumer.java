52c043ee3707bd2eb793b4b1dd264a674d60e52a
Dinh Viet Thanh <dvthanh19@gmail.com>
2024-07-08 13:49:14 +0700

    ...

--- src/main/java/protobuf/Consumer.java
+++ src/main/java/protobuf/Consumer.java
@@ -5,6 +5,7 @@ import protobuf.FileSchema.*;
 import protobuf.GenerateMD5;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -29,10 +30,9 @@ public class Consumer {
     
     private static final String BOOTSTRAP_SERVERS = "localhost:9092";
     private static final String TOPIC_NAME = "video-topic";
-    private static long expectedChunk = 0;
-    private static HashMap<String, LinkedList<Datachunk>> fileMap = new HashMap<>();
+    private static HashMap<String, LinkedList<Datachunk>> chunkListMap = new HashMap<>();
     private static HashMap<String, FileOutputStream> outputStreamMap = new HashMap<>();
-    private static LinkedList<Datachunk> chunkList = new LinkedList<>();
+    // private static LinkedList<Datachunk> chunkList = new LinkedList<>();
 
 
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException
@@ -40,26 +40,32 @@ public class Consumer {
         // -------------------------------------------------------------------------------------------
         // GET INFORMATION ABOUT SENT FILES ----------------------------------------------------------
         String trackFilePath = "D:\\server\\trackingFile.txt";
-        HashMap<String, String> trackFile = null;
+        HashMap<String, String> trackFiles = null;
         Stack<String> stack = new Stack<>();
+        int stackSize = 0;
         
-        try {trackFile = readFromSerializedFile(trackFilePath);} 
+        try {trackFiles = readFromSerializedFile(trackFilePath);} 
         catch (IOException e) 
         {System.err.println("Error reading file: " + e.getMessage());} 
         catch (ClassNotFoundException e)
         {System.err.println("Error: Class not found during deserialization: " + e.getMessage());}
 
 
-        for (String file : trackFile.keySet())
-            if (trackFile.get(file).charAt(0) != '1')
+        for (String file : trackFiles.keySet())
+            System.out.println("File <" + file + ">         " + trackFiles.get(file));
+            System.out.println("      -----------      ");
+        // Push all file with status xy (x!=1) into stack
+        for (String file : trackFiles.keySet())
+            if (trackFiles.get(file).charAt(0) != '1')
                 stack.push(file);
         
-        int stackSize = stack.size();
+        // Remove all file with status xy (x!=1) out of hashMap
+        stackSize = stack.size();
         for (int i = 0; i < stackSize; i += 1)
-            trackFile.remove(stack.pop());
+            trackFiles.remove(stack.pop());
 
-        // for (String file : trackFile.keySet())
-        //     System.out.println("File <" + file + ">         " + trackFile.get(file));
+        for (String file : trackFiles.keySet())
+            System.out.println("File <" + file + ">         " + trackFiles.get(file));
 
 
 
@@ -68,14 +74,32 @@ public class Consumer {
 
         // String filePath = "D:\\dir_server\\" + fileName + fileExt;
         String dir = "D:\\dir_server\\"; 
-
+        HashMap<String, Long> chunknumMap = new HashMap<>();
+        HashMap<String, Long> countMap = new HashMap<>();
+        HashMap<String, Long> expectedChunkMap = new HashMap<>();
+        HashMap<String, Boolean> isFirstMap = new HashMap<>();
+        HashMap<String, Boolean> receiveMap = new HashMap<>();
+        boolean fun = true;
+        FileOutputStream outputStream1 = new FileOutputStream(new File(dir + "test.txt"));;
         
-        for (String file : trackFile.keySet())
+        for (String file : trackFiles.keySet())
         {
             File outputFile = new File(dir + file);
+            System.out.println(dir + file);
             try (FileOutputStream outputStream = new FileOutputStream(outputFile)) {
-                outputStreamMap.put(file, outputStream);
+                // outputStreamMap.put(file, outputStream);
+                // outputStream1 = new FileOutputStream(outputFile);
+
             } catch (IOException e) {e.printStackTrace();}
+
+            chunknumMap.put(file, (long) 0);
+            countMap.put(file, (long) 0);
+            expectedChunkMap.put(file, (long) 0);
+
+            isFirstMap.put(file, true);
+            receiveMap.put(file, false);
+
+            chunkListMap.put(file, new LinkedList<>());
         }
 
 
@@ -94,21 +118,9 @@ public class Consumer {
         consumer.subscribe(Collections.singletonList(TOPIC_NAME));
 
 
-        
-
+        String fileName;
         while (true) {
-            boolean fun = true;
-            HashMap<String, Integer> chunknumMap = new HashMap<>();
-            HashMap<String, Integer> countMap = new HashMap<>();
-            HashMap<String, Boolean> isFirstMap = new HashMap<>();
-            for (String file : trackFile.keySet())
-            {
-                chunknumMap.put(file, 0);
-                countMap.put(file, 0);
-                isFirstMap.put(file, false);
-            }
-
-
+            // System.out.println("...1");
             ConsumerRecords<Long, byte[]> records = consumer.poll(Duration.ofMillis(50));
 
             if (fun) 
@@ -121,9 +133,10 @@ public class Consumer {
             {
                 byte[] message = record.value();
                 Datachunk chunk = Datachunk.parseFrom(message);
-                String fileName = chunk.getFileName() + chunk.getFileExt();
+                fileName = chunk.getFileName() + chunk.getFileExt();
 
-                if (isFirstMap.get(fileName)) {
+                if (isFirstMap.get(fileName))
+                {
                     chunknumMap.put(fileName, chunk.getChunkNum());
                     isFirstMap.put(fileName, false);
                 }
@@ -132,66 +145,89 @@ public class Consumer {
                 System.out.printf("Received message: offset = %d, partition = %d, message = %s\n",
                         record.offset(), record.partition(), chunk);
 
-        //         if (expectedChunk == Long.parseLong(chunk.getChunkID()))
-        //         {
-        //             count += 1;
-        //             outputStream.write(chunk.getBody().toByteArray());
-        //             expectedChunk += 1;
+                if (expectedChunkMap.get(fileName) == Long.parseLong(chunk.getChunkID()))
+                {
+                    System.out.println("... 1");
+                    countMap.put(fileName, countMap.get(fileName) + 1);
+                    // outputStreamMap.get(fileName).write(chunk.getBody().toByteArray());
+                    outputStream1.write(chunk.getBody().toByteArray());
+                    System.out.println("... 2");
+                    expectedChunkMap.put(fileName, expectedChunkMap.get(fileName) + 1);
                     
-        //             if (chunkList.size() > 0)
-        //             {
-        //                 Datachunk tmp = chunkList.getFirst();
-        //                 if (expectedChunk == Long.parseLong(tmp.getChunkID()))
-        //                 {
-        //                     while (expectedChunk == Long.parseLong(tmp.getChunkID()))
-        //                     {
-        //                         outputStream.write(tmp.getBody().toByteArray());
-        //                         chunkList.removeFirst();
 
-        //                         expectedChunk += 1;
-        //                         if (chunkList.size() > 0)
-        //                         {
-        //                             tmp = chunkList.getFirst();
-        //                         }
-        //                     }
-        //                 }
-        //             }
-        //         }
-        //         else
-        //         {
-        //             // count += 1; //  ???
-        //             boolean successFlag = addChunkIntoList(chunk);
-        //             if (successFlag == true){
-        //                 count += 1; //  ???
-        //             }
-        //         }
-        //         System.out.println("count: " + count + ",      expected: " + expectedChunk  + ",      size: " + chunkList.size());
+                    if (chunkListMap.get(fileName).size() > 0)
+                    {
+                        Datachunk tmp = chunkListMap.get(fileName).getFirst();
+                        if (expectedChunkMap.get(fileName) == Long.parseLong(tmp.getChunkID()))
+                        {
+                            while (expectedChunkMap.get(fileName) == Long.parseLong(tmp.getChunkID()))
+                            {
+                                outputStreamMap.get(fileName).write(tmp.getBody().toByteArray());
+                                chunkListMap.get(fileName).removeFirst();
+
+                                expectedChunkMap.put(fileName, expectedChunkMap.get(fileName) + 1);
+                                if (chunkListMap.get(fileName).size() > 0)
+                                {
+                                    tmp = chunkListMap.get(fileName).getFirst();
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    // count += 1; //  ???
+                    boolean successFlag = addChunkIntoList(chunk, chunkListMap.get(fileName));
+                    if (successFlag == true){
+                        countMap.put(fileName, countMap.get(fileName) + 1); //  ???
+                    }
+                }
+                System.out.println("count: " + countMap.get(fileName) + ",      expected: " + expectedChunkMap.get(fileName)  + ",      llsize: " + chunkListMap.get(fileName).size());
                 
-        //     }
-        //     if (chunknum == count && !isFirst) {
-        //         System.out.println("Done receiving: " + String.valueOf(chunknum) + "/" + String.valueOf(chunknum));
-
-        //         int listSize = chunkList.size();
-        //         for (int i = 0; i < listSize; i += 1)
-        //         {
-        //             expectedChunk += 1;
-        //             Datachunk tmp = chunkList.getFirst();
-        //             outputStream.write(tmp.getBody().toByteArray());
-
-        //             chunkList.removeFirst();
-        //         }
-
-        //         System.out.println("-- expected: " + expectedChunk);
-        //         break;
             }
-        }
-        consumer.close();
 
+            int count = 0;
+            // System.out.println("...2");
+            for (String file : trackFiles.keySet())
+            {   
+                if (!receiveMap.get(file))
+                {
+                    System.out.println(chunknumMap.get(file) + "    " + countMap.get(file) + "    " + !isFirstMap.get(file));// ----
+                    if (chunknumMap.get(file) == countMap.get(file) && !isFirstMap.get(file)) {
+                        System.out.println("...2.2   " + chunknumMap.get(file));
+                        count += 1;
+                        
+
+                        System.out.println("Done receiving: " + String.valueOf(countMap.get(file)) + "/" + String.valueOf(chunknumMap.get(file)));
         
+                        int listSize = chunkListMap.get(file).size();
+                        for (int i = 0; i < listSize; i += 1)
+                        {
+                            expectedChunkMap.put(file, expectedChunkMap.get(file) + 1) ;
+                            Datachunk tmp = chunkListMap.get(file).getFirst();
+                            outputStreamMap.get(file).write(tmp.getBody().toByteArray());
+        
+                            chunkListMap.get(file).removeFirst();
+                        }
+        
+                        System.out.println("-- expected: " + expectedChunkMap.get(file));
+                        receiveMap.put(file, true);
+                    }
+                }
+                else {count += 1;}
+            }
+            // System.out.println("...3");
+
+            System.out.println(count + "   " + trackFiles.size());
+            if (count == trackFiles.size()) break;
+        }
+
+
+        consumer.close();
     }
 
 
-    private static boolean addChunkIntoList(Datachunk chunk)
+    private static boolean addChunkIntoList(Datachunk chunk, LinkedList<Datachunk> chunkList)
     {
         int size = chunkList.size();
         if (size == 0)
@@ -206,11 +242,9 @@ public class Consumer {
         // int i = size/2;
         // int k = 0;
         // Datachunk tmp = chunkList.get(size/2);
-
         // if (id < Long.parseLong(tmp.getChunkID())) 
         //     k = -1;
         // else k = 1;
-
         // for (;i >= 0 && i < chunkList.size(); i += k)
         // {
         //     tmp = chunkList.get(i);
@@ -224,7 +258,6 @@ public class Consumer {
         //         break;
         //     }
         // }
-
         // if (i < 0) // The chunkID is smaller than headID
         // { 
         //     return false;
